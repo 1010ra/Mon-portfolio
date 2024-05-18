61ded0f66e4a2186b919f33aed27ce1af752d0fc
rakia <rakiaa140gmail.com>
2024-05-19 00:41:43 +0100

    button toggle emailJs formilaire

--- portfolio.js
+++ portfolio.js
@@ -10,5 +10,36 @@ function typeWriter() {
   }
 }
 
+typeWriter()
 
-    typeWriter()
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Ces informations seront envoyées à EmailJS
+    const nom = document.getElementById('inputNom').value;
+    const email = document.getElementById('inputMail').value;
+    const sujet = document.getElementById('inputSujet').value;
+    const details = document.querySelector('textarea').value;
+
+    const templateParams = {
+        name: nom,
+        email: email,
+        message: sujet,
+        details: details
+    };
+
+    emailjs.send('service_ylwkkjr', 'template_tjbvmwr', templateParams)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Merci! Votre message a été envoyé.');
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert('Erreur! Votre message n\'a pas pu être envoyé.');
+        });
+});
+
+    
+
+
+
